a43791ac6d88907e4c772745b140d4bbe47c9db4
Sofia Vognild Burkow <s.v.burkow@student.tudelft.nl>
2025-12-07 10:01:42 +0100

    output feature importance

--- experiments/models/train_decision_tree.py
+++ experiments/models/train_decision_tree.py
@@ -3,6 +3,17 @@ from sklearn import tree
 from helper_func import load_datasets, train_and_test_classifier
 
 
+def print_feature_importances(clf, feature_names, top_k=7):
+    importance = clf.feature_importances_
+    idx = importance.argsort()[::-1][:top_k]
+
+    print(f"Top {top_k} feature indices: {idx}")
+    print(f"Number of features: {len(feature_names)}")
+
+    for i in idx:
+        print(f"{feature_names[i]}: {importance[i]:.4f}")
+
+
 def train_and_test_decision_tree(data_split_mode):
 
     # Load datasets
@@ -28,6 +39,27 @@ def train_and_test_decision_tree(data_split_mode):
     print("Confusion Matrix:")
     print(cm)
 
+    # feature_names = [   
+    #     'numerical__duration', 'numerical__orig_bytes', 'numerical__resp_bytes',
+    #     'numerical__orig_pkts', 'numerical__resp_pkts', 'categorical__proto_icmp',
+    #     'categorical__proto_tcp', 'categorical__proto_udp',
+    #     'categorical__service_-', 'categorical__service_dce_rpc,smb',
+    #     'categorical__service_dns', 'categorical__service_finger',
+    #     'categorical__service_ftp', 'categorical__service_ftp-data',
+    #     'categorical__service_http', 'categorical__service_ntp',
+    #     'categorical__service_pop3', 'categorical__service_smb',
+    #     'categorical__service_smtp', 'categorical__service_snmp',
+    #     'categorical__service_ssh', 'categorical__service_syslog',
+    #     ]
+
+    feature_names = [
+        'numerical__duration', 'numerical__orig_bytes', 'numerical__resp_bytes',
+        'numerical__orig_pkts', 'numerical__resp_pkts', 'categorical__proto_icmp',
+        'categorical__proto_tcp', 'categorical__proto_udp',
+        ]
+    
+    print_feature_importances(clf, feature_names, top_k=5)
+
 
 if __name__ == "__main__":
     # Command: uv run python experiments/models/train_decision_tree.py <split|insidedmz|scenarios>
@@ -43,4 +75,3 @@ if __name__ == "__main__":
         sys.exit(1)
 
     train_and_test_decision_tree(data_split_mode)
-
