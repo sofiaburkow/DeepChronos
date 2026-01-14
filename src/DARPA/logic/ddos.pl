44c1d74c509cd0631dbde8027c8ee69a0176588f
Sofia Vognild Burkow <s.v.burkow@student.tudelft.nl>
2026-01-14 14:28:08 +0100

    improved logic

--- src/DARPA/logic/ddos.pl
+++ src/DARPA/logic/ddos.pl
@@ -1,11 +1,8 @@
-nn(net5, [X], Z, [benign, mal]) :: phase5(X, Z).
+nn(net5, [X], Z, [benign, malicious]) :: phase5(X, Z).
 
 ddos(X, P1, P2, P3, P4, alarm) :-
-    P1 = 1, P2 = 1, P3 = 1, P4 = 1,
-    phase5(X, mal).
+    4 is P1 + P2 + P3 + P4,
+    phase5(X, malicious).
 
 ddos(X, P1, P2, P3, P4, no_alarm) :-
-    P1\=1 ; P2\=1 ; P3\=1 ; P4\=1.
-
-ddos(X, P1, P2, P3, P4, no_alarm) :-
-    phase5(X, benign).
+    \+ ddos(X, P1, P2, P3, P4, alarm).
