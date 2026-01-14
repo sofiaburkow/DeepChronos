3e653d454ca8a63661457b9707f287d309944dbe
Sofia Vognild Burkow <s.v.burkow@student.tudelft.nl>
2026-01-14 15:26:32 +0100

    some more tweaks to the logic

--- src/DARPA/logic/ddos.pl
+++ src/DARPA/logic/ddos.pl
@@ -1,8 +1,16 @@
+% Works with pretrained net5
 nn(net5, [X], Z, [benign, malicious]) :: phase5(X, Z).
 
+phases_complete(P1, P2, P3, P4) :-
+    4 is P1 + P2 + P3 + P4.
+
 ddos(X, P1, P2, P3, P4, alarm) :-
-    4 is P1 + P2 + P3 + P4,
+    phases_complete(P1, P2, P3, P4),
     phase5(X, malicious).
 
 ddos(X, P1, P2, P3, P4, no_alarm) :-
-    \+ ddos(X, P1, P2, P3, P4, alarm).
+    phases_complete(P1, P2, P3, P4),
+    phase5(X, benign).
+
+ddos(X, P1, P2, P3, P4, no_alarm) :-
+    P1 + P2 + P3 + P4 < 4.
--- src/DARPA/logic/ddos_alt.pl
+++ src/DARPA/logic/ddos_alt.pl
@@ -1,4 +1,3 @@
-% Works with pretrained net5
 nn(net5, [X], Z, [benign, malicious]) :: phase5(X, Z).
 
 ddos(X, P1, P2, P3, P4, alarm) :-
@@ -6,8 +5,4 @@ ddos(X, P1, P2, P3, P4, alarm) :-
     phase5(X, malicious).
 
 ddos(X, P1, P2, P3, P4, no_alarm) :-
-    4 is P1 + P2 + P3 + P4,
-    phase5(X, benign).
-
-ddos(X, P1, P2, P3, P4, no_alarm) :-
-    P1 + P2 + P3 + P4 < 4.
+    \+ ddos(X, P1, P2, P3, P4, alarm). % only symbolic reasoning
