01bbd0cc793ebbe2c6a9f12fdae884d5076c6e42
Sofia Vognild Burkow <s.v.burkow@student.tudelft.nl>
2026-03-16 13:55:41 +0100

    more promising results

--- src/deepproblog/models/darpa_flags_ports_ips.pl
+++ src/deepproblog/models/darpa_flags_ports_ips.pl
@@ -1,29 +1,71 @@
-:- use_module('src/deepproblog/models/logic.py').
-
 % Neural networks
+
 nn(net1, [X], Z, [benign, phase1]) :: recon(X, Z).
 nn(net2, [X], Z, [benign, phase2]) :: ping(X, Z).
 nn(net3, [X], Z, [benign, phase3]) :: overflow(X, Z).
 nn(net4, [X], Z, [benign, phase4]) :: install(X, Z).
 nn(net5, [X], Z, [benign, phase5]) :: ddos(X, Z).
 
-% Vulnerability knowledge
+% Expert knowledge
 
-sadmind_known_port(111).
-sadmind_known_port(Port) :- Port >= 32771.
-sadmind_port(P) :- sadmind_known_port(P).
+% Port knowledge
+c2_port(514).
+c2_port(1022).
+download_port(23).
 
-download_port(514).
-download_port(1022).
-
+% Protocol knowledge
 icmp(1).
 tcp(6).
 udp(17).
 
+% Origin knowledge
 loc_orig(1).
 loc_resp(1).
 ext_orig(0).
 ext_resp(0).
+
+% Vulnerability knowledge
+sadmind_known_port(111).
+sadmind_known_port(Port) :- Port >= 32771.
+sadmind_port(P) :- sadmind_known_port(P).
+sadmind_followup_port(23).
+
+% Attack phase rules
+
+sadmind_scan(SO, DO, DPort, Proto) :-
+    ext_orig(SO),
+    loc_resp(DO),
+    sadmind_port(DPort),
+    udp(Proto).
+
+sadmind_vuln(SO, DO, DPort, Proto) :-
+    ext_orig(SO),
+    loc_resp(DO),
+    sadmind_followup_port(DPort),
+    tcp(Proto).
+
+recon_response(SO, DO, Proto) :-
+    loc_resp(SO),
+    ext_orig(DO),
+    icmp(Proto).
+
+c2_contact(SO, DO, DPort, Proto) :-
+    loc_orig(SO),
+    ext_orig(DO),
+    c2_port(DPort),
+    tcp(Proto).
+
+download_mal(SO, DO, DPort, Proto) :-
+    ext_orig(SO),
+    loc_resp(DO),
+    download_port(DPort),
+    tcp(Proto).
+
+ddos_pattern(SO, Proto) :-
+    ext_orig(SO),
+    tcp(Proto).
+
+
 
 % Multi-step attack logic
 
@@ -37,36 +79,29 @@ phase(1, X, _, _, _, _, Pred) :-
 
 
 phase(2, _, SO, DO, DPort, Proto, phase2) :-
-    ext_orig(SO),
-    loc_resp(DO),
-    sadmind_port(DPort),
-    udp(Proto).
-
-phase(2, _, SO, DO, DPort, Proto, phase2) :-
-    loc_resp(SO),
-    ext_orig(DO),
-    sadmind_port(DPort),
-    icmp(Proto).
+    sadmind_scan(SO, DO, DPort, Proto);
+    recon_response(SO, DO, Proto).
 
 phase(2, X, _, _, _, _, Pred) :-
     ping(X, Pred).
 
 
-phase(3, X, _, _, VictimPort, _, phase3) :- 
-    sadmind_port(VictimPort).
+phase(3, X, SO, DO, VictimPort, _, phase3) :- 
+    sadmind_scan(SO, DO, VictimPort, Proto);
+    sadmind_vuln(SO, DO, VictimPort, Proto).
 
 phase(3, X, _, _, _, _, Pred) :-
     overflow(X, Pred).
 
-
-phase(4, X, _, _, DPort, _, phase4) :-
-    download_port(DPort).
+phase(4, X, SO, DO, DPort, Proto, phase4) :-
+    c2_contact(SO, DO, DPort, Proto);
+    download_mal(SO, DO, DPort, Proto).
 
 phase(4, X, _, _, _, _, Pred) :-
     install(X, Pred).
 
-
 phase(5, X, _, _, _, _, Pred) :-
+    ddos_pattern(X, Pred);
     ddos(X, Pred).
 
 
